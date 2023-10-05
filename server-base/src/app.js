a1106884faee4ec10d514c79776013c5c2464a7e
ejrojasp <ejrojasp@falabella.cl>
2023-10-05 04:02:56 -0300

    Added static file support to server base.

--- server-base/src/app.js
+++ server-base/src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import indexRouter from './routers/index.router.js';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 
